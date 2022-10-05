c08b3d2c0abbbf96a87d467da0a599eb75e7e9ff
Jack <1162858020@qq.com>
2022-10-05 14:20:54 +0800

    修复了无法用yarn build打开的bug

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/morney-3-website/'
-    : '/',
+  publicPath: '/',
   lintOnSave: false,
   chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icons')
