7dd8a5b9cb24436a1bebed6cc6d9ba6f9d1f3429
Jack <1162858020@qq.com>
2022-10-05 15:31:13 +0800

    实现了github部署

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:FrankFang/morney-3-website.git master:gh-pages
+git push -f git@github.com:k93846/morney-web.git master:gh-pages
 
 cd -
